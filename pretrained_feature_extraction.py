0e801bea074a9e15f26bb4b2b0130f37d2a33a21
shackla <35693550+shackla@users.noreply.github.com>
2018-08-16 15:36:09 +0100

    Add files via upload

---
+++ pretrained_feature_extraction.py
@@ -0,0 +1,105 @@
+# this example will use a pretrained..
+# image classifier,the VGG2016 model to be exact
+# it is a model trained on the ImageNet data base..
+# trained on 1.4 million images with 1000 classes,
+# thus we can expect to handle a binary classification problem...
+# very well, it is quite a bit more bulky than the state of art..
+# image classifiers that are around nowaday, but has a very simlar structure...
+# to the convnet models we've seen up to this
+
+# this example will focus use a pretrained network using feature extraction
+
+# image classifer convnets come in two parts
+# 1) convolutional base - these are the layers of convfilters and maxpools
+# 2) densely connected classifier- dense layers at the end of the models
+# the convolutional base can be reused as in is much more likely to be...
+# more generic therefore more reusable, classifier are more data specifc
+# so changing them from model to model is a good idea
+
+from keras.applications import VGG16
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import models
+from keras import layers
+from keras import optimizers
+import matplotlib.pyplot as plt
+
+base_dir = '/home/alex/Documents/MAI/Deep Learning/cats_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+# when defining the pretrained model:
+# weights- indicates the weight checkpoint to initialse the model at
+# include_top - whether the pretrained densely connected classifer is included
+# input_shape - shape of input tensor
+
+
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size:(i + 1) * batch_size] = features_batch
+        labels[i * batch_size:(i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+history = model.fit(train_features, train_labels,
+                    epochs=30,
+                    batch_size=20,
+                    validation_data=(validation_features, validation_labels))
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
