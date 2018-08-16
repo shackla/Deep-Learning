8ac859b4033711d19997c8746d0deac27c90e13c
shackla <35693550+shackla@users.noreply.github.com>
2018-08-16 16:35:00 +0100

    Update layer_visualisation.py

--- layer_visualisation.py
+++ layer_visualisation.py
@@ -10,7 +10,9 @@ model.summary()
 
 base_dir = '/home/alex/Documents/MAI/Deep Learning/cats_dogs_small'
 
-img_path = os.path.join(base_dir, 'test/dogs/dog.1504.jpg')
+img_path = os.path.join(base_dir, 'test/dogs/dog.1916.jpg')
+
+# loading image and converting to a tensor
 
 img = image.load_img(img_path, target_size=(150, 150))
 img_tensor = image.img_to_array(img)
@@ -18,17 +20,24 @@ img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255.
 
 print(img_tensor.shape)
-
 plt.imshow(img_tensor[0])
 
+# layer_outputs - extracts the outputs of the top 8 layer
+# activation_model - creates a model that will return the outputs..
+# given the model input
 
 layer_outputs = [layer.output for layer in model.layers[:8]]
 activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
 
 activations = activation_model.predict(img_tensor)
 
+# shows off what the 4th filter in the 1st layer is looking for..
+# looks like it detects diagonal lines
+
 first_layer_activation = activations[0]
-plt.matshow(first_layer_activation[0, :, :, 1], cmap='viridis')
+plt.matshow(first_layer_activation[0, :, :, 4], cmap='viridis')
+
+# this part of the code prints out all of the different filters for all layers
 
 layer_names = []
 for layer in model.layers[:8]:
@@ -64,3 +73,11 @@ for layer_name, layer_activation in zip(layer_names, activations):
     plt.grid(False)
     plt.imshow(display_grid, aspect='auto', cmap='viridis')
 plt.show()
+
+# the first layer of activation acts as an edge detector
+# as you go deeper the layers become less visual and more...
+# about the class of the image
+# its not detect visual aspects of image, its dealling with...
+# more abstract class specfic aspects i.e ears, noses
+# also as you do down the layers, less of them arre activated...
+# meaning that encoded pattern they have is not being found in the input
